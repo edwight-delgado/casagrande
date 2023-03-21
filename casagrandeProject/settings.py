f2ee01409798ee6ad43a69cec48f40249784aab1
edwight <edwightdelgado@gmail.com>
2023-03-21 11:14:54 -0300

    add mysql

--- casagrandeProject/settings.py
+++ casagrandeProject/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = "casagrandeProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'railway',
+            'USER': 'root',
+            'PASSWORD': 'ZAKPBw0zgLLsqoZy6AGn',
+            'HOST': 'containers-us-west-167.railway.app',
+            'PORT': '5729',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
